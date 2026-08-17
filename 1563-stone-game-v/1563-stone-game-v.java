fe4de8c96a70b1939488f8052548aba1a77072f8
Tarang Harsola <tarang.harsola@gmail.com>
2026-08-17 19:29:06 +0530

    Time: 17 ms (99.76%), Space: 47.6 MB (76.68%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -8,28 +8,48 @@ class Solution {
             prefix[i + 1] = prefix[i] + stoneValue[i];
         }
 
-        int[][] dp = new int[n][n];
+        int[] dp = new int[n + 1];
+        int[][] suffixMax = new int[n + 1][n + 1];
 
-        for (int len = 2; len <= n; len++) {
-            for (int l = 0; l + len <= n; l++) {
-                int r = l + len - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            suffixMax[i + 1][i + 1] = Integer.MIN_VALUE;
+            suffixMax[i][i + 1] = -prefix[i];
 
-                for (int k = l; k < r; k++) {
-                    int left = prefix[k + 1] - prefix[l];
-                    int right = prefix[r + 1] - prefix[k + 1];
+            int prefixMax = 0;
+            int k = i + 1;
 
-                    if (left < right) {
-                        dp[l][r] = Math.max(dp[l][r], left + dp[l][k]);
-                    } else if (left > right) {
-                        dp[l][r] = Math.max(dp[l][r], right + dp[k + 1][r]);
-                    } else {
-                        dp[l][r] = Math.max(dp[l][r],
-                            Math.max(left + dp[l][k], right + dp[k + 1][r]));
-                    }
+            for (int j = i + 2; j <= n; j++) {
+                while (k < j &&
+                       prefix[k] - prefix[i] <= prefix[j] - prefix[k]) {
+
+                    prefixMax = Math.max(
+                        prefixMax,
+                        dp[k] + prefix[k]
+                    );
+
+                    k++;
                 }
+
+                int q;
+
+                if (prefix[k - 1] - prefix[i] == prefix[j] - prefix[k - 1]) {
+                    q = k - 1;
+                } else {
+                    q = k;
+                }
+
+                dp[j] = Math.max(
+                    prefixMax - prefix[i],
+                    suffixMax[q][j] + prefix[j]
+                );
+
+                suffixMax[i][j] = Math.max(
+                    suffixMax[i + 1][j],
+                    dp[j] - prefix[i]
+                );
             }
         }
 
-        return dp[0][n - 1];
+        return dp[n];
     }
 }
