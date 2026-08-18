82d865964ceb0d80e8f409c626cb3d7f16bc19f2
Tarang Harsola <tarang.harsola@gmail.com>
2026-08-18 10:18:51 +0530

    Time: 1 ms (99.3%), Space: 44.1 MB (99.3%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,34 +1,62 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
         int n = nums.length;
-        int[] freq = new int[51];
-        int[] count = new int[51];
 
-        for (int i = 0; i < k; i++) {
-            freq[nums[i]]++;
-        }
+        if(n == k) {
+            int max = nums[0];
 
-        for (int x = 0; x <= 50; x++) {
-            if (freq[x] > 0) {
-                count[x]++;
+            for(int i = 1; i < n; i++) {
+                max = Math.max(max, nums[i]);
             }
+
+            return max;
         }
 
-        for (int i = k; i < n; i++) {
-            freq[nums[i - k]]--;
-            freq[nums[i]]++;
+        if(k == 1) {
+            int[] freq = new int[51];
 
-            for (int x = 0; x <= 50; x++) {
-                if (freq[x] > 0) {
-                    count[x]++;
+            for(int x : nums) {
+                freq[x]++;
+            }
+
+            int max = -1;
+
+            for(int x : nums) {
+                if(freq[x] == 1) {
+                    max = Math.max(max, x);
                 }
             }
+
+            return max;
         }
 
-        for (int x = 50; x >= 0; x--) {
-            if (count[x] == 1) {
-                return x;
+        if(nums[0] == nums[n - 1]) {
+            return -1;
+        }
+
+        boolean first = true;
+        boolean last = true;
+
+        for(int i = 1; i < n - 1; i++) {
+            if(nums[i] == nums[0]) {
+                first = false;
             }
+
+            if(nums[i] == nums[n - 1]) {
+                last = false;
+            }
+        }
+
+        if(first && last) {
+            return Math.max(nums[0], nums[n - 1]);
+        }
+
+        if(first) {
+            return nums[0];
+        }
+
+        if(last) {
+            return nums[n - 1];
         }
 
         return -1;
