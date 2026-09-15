bb00cbb28b082ac2eb3e6edd9ff32dbc9c14bae0
Tarang Harsola <tarang.harsola@gmail.com>
2026-09-15 20:23:45 +0530

    Time: 6 ms (98.41%), Space: 44.6 MB (71.46%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,22 +1,43 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int n = img1.length;
-        Map<List<Integer>, Integer> cnt = new HashMap<>();
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (img1[i][j] == 1) {
-                    for (int h = 0; h < n; ++h) {
-                        for (int k = 0; k < n; ++k) {
-                            if (img2[h][k] == 1) {
-                                List<Integer> t = List.of(i - h, j - k);
-                                ans = Math.max(ans, cnt.merge(t, 1, Integer::sum));
-                            }
-                        }
-                    }
-                }
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i] |= img1[i][j] << j;
+                b[i] |= img2[i][j] << j;
             }
         }
+
+        int ans = 0;
+
+        for (int dr = -(n - 1); dr <= n - 1; dr++) {
+            for (int dc = -(n - 1); dc <= n - 1; dc++) {
+                int overlap = 0;
+
+                for (int i = 0; i < n; i++) {
+                    int j = i + dr;
+
+                    if (j < 0 || j >= n) continue;
+
+                    int x = a[i];
+                    int y = b[j];
+
+                    if (dc > 0) {
+                        x <<= dc;
+                    } else {
+                        y <<= -dc;
+                    }
+
+                    overlap += Integer.bitCount(x & y);
+                }
+
+                ans = Math.max(ans, overlap);
+            }
+        }
+
         return ans;
     }
 }
