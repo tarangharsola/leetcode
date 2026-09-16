e9c633ae39bb13098ccf1370f7df6eda5f5f9bbe
Tarang Harsola <tarang.harsola@gmail.com>
2026-09-16 14:37:21 +0530

    Time: 0 ms (100%), Space: 43.6 MB (52.39%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,20 +15,16 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null)
-        {
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == root2) {
             return true;
         }
-        return isMirror(root.left,root.right);
-        
-    }
-    public boolean isMirror(TreeNode t1,TreeNode t2)
-    {
-        if(t1 == null && t2 == null)return true;
-        if(t1 == null || t2 == null)return false;
-        if(t1.val != t2.val) return false;
-
-        return isMirror(t1.left,t2.right) &&
-        isMirror(t1.right, t2.left);
+        if (root1 == null || root2 == null || root1.val != root2.val) {
+            return false;
+        }
+        return dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
     }
 }
